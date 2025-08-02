0c9ab2c69410a60e33567f26ddfbf4be4ff75655
al-jabir <aljabir098@gmail.com>
2025-08-02 20:11:08 +0600

    Initialize 3D gaming project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-center text-orange-500">Welcome to My 3D Gaming Web</h1>
+    </main>
+  );
+};
+
+export default App;
